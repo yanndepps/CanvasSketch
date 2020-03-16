9796886523fb48d8cea74079e8d2d1f4f6abec90
Yann Depps <yann.deperrot@protonmail.com>
2020-03-16 23:44:55 +0100

    wip gray klein

--- Sketches/webgl_03/gray_klein_bottle.js
+++ Sketches/webgl_03/gray_klein_bottle.js
@@ -1,5 +1,5 @@
 // @ts-nocheck
-// 20:00
+// 42:20
 // Ensure ThreeJS is in global scope for the 'examples/'
 global.THREE = require("three");
 
@@ -26,13 +26,53 @@ const planeFunction = (u, v, target) => {
   target.set(x, y, z);
 };
 
-const sphereFunction = (u, v, target) => {
+const kleinFunction = (u, v, target) => {
+  let a = 1;
   u *= Math.PI * 2;
-  v *= Math.PI;
+  v *= Math.PI * 2;
 
-  let x = (0.5 + 0.5 * Math.sin(u * 10.)) * Math.sin(u) * Math.sin(v);
-  let y = (0.5 + 0.5 * Math.sin(u * 10.)) * Math.sin(u) * Math.cos(v);
-  let z = (0.5 + 0.5 * Math.sin(u * 10.)) * Math.cos(u);
+  let x = Math.cos(u) * (a + Math.sin(v) * Math.cos(u / 2) - Math.sin(2 * v) * Math.sin(u / 2) / 2);
+  let y = Math.sin(u) * (a + Math.sin(v) * Math.cos(u / 2) - Math.sin(2 * v) * Math.sin(u / 2) / 2);
+  let z = Math.sin(u / 2) * Math.sin(v) + Math.cos(u / 2) * Math.sin(2 * v) / 2;
+
+  target.set(x, y, z);
+};
+
+const graykleinFunction = (u, v, target) => {
+
+  u *= Math.PI * 4;
+  v *= Math.PI * 2;
+
+  let a = 3;
+  let n = 3;
+  let m = 1;
+
+  let x = (a + Math.cos(n * u / 2.0) * Math.sin(v) - Math.sin(n * u / 2.0) * Math.sin(2 * v)) * Math.cos(m * u / 2.0);
+  let y = (a + Math.cos(n * u / 2.0) * Math.sin(v) - Math.sin(n * u / 2.0) * Math.sin(2 * v)) * Math.sin(m * u / 2.0);
+  let z = Math.sin(n * u / 2.0) * Math.sin(v) + Math.cos(n * u / 2.0) * Math.sin(2 * v);
+
+  target.set(x, y, z);
+};
+
+const sphereFunction = (u, v, target) => {
+  u *= Math.PI;
+  v *= -Math.PI * 2;
+
+  let x = Math.sin(u) * Math.sin(v);
+  let y = Math.sin(u) * Math.cos(v);
+  let z = Math.cos(u);
+
+  target.set(x, y, z);
+};
+
+const torFunction = (u, v, target) => {
+  u *= Math.PI * 2;
+  v *= Math.PI * 2;
+  u -= -Math.PI;
+
+  let x = Math.cos(v) + Math.sin(u) * Math.cos(v);
+  let y = Math.sin(v) + Math.sin(u) * Math.sin(v);
+  let z = Math.cos(u);
 
   target.set(x, y, z);
 };
@@ -47,7 +87,7 @@ const sketch = ({ context }) => {
 
   // Setup a camera
   const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
-  camera.position.set(0, 0, -4);
+  camera.position.set(0, 0, 10);
   camera.lookAt(new THREE.Vector3());
 
   // Setup camera controller
@@ -59,7 +99,9 @@ const sketch = ({ context }) => {
   // Setup a geometry
   //const geometry = new THREE.PlaneGeometry(1, 1, 10, 10);
 
-  const geometry = new THREE.ParametricBufferGeometry(sphereFunction, 25, 25);
+  const geometry = new THREE.ParametricBufferGeometry(torFunction, 100, 100);
+  const geometry1 = new THREE.ParametricBufferGeometry(graykleinFunction, 100, 100);
+  geometry.setAttribute('position1', new THREE.BufferAttribute(geometry1.attributes.position.array, 3));
 
 
 
@@ -68,11 +110,18 @@ const sketch = ({ context }) => {
     // ---
     varying vec2 vUv;
     varying vec3 vPosition;
+    varying vec3 vNormal;
+    attribute vec3 position1;
     uniform float time;
 
     void main() {
       vPosition = position;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      vNormal = normal;
+      vUv = uv;
+
+      vec3 final = mix(position, position1, 0.5 + 0.5 * sin(time));
+
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(final, 1.0);
     }
   `;
 
@@ -80,11 +129,19 @@ const sketch = ({ context }) => {
   const fragment = /* glsl */ `
     varying vec2 vUv;
     varying vec3 vPosition;
+    varying vec3 vNormal;
     uniform float time;
-    uniform vec3 color;
+    //uniform vec3 color;
 
     void main() {
-      // --
+      float diff = abs(dot(normalize(vec3(1.,1.,1.)), vNormal));
+
+      vec3 a = vec3(0.5, 0.5, 0.5);
+      vec3 b = vec3(0.5, 0.5, 0.5);
+      vec3 c = vec3(2.0, 1.0, 0.0);
+      vec3 d = vec3(0.5, 0.2, 0.25);
+
+      vec3 color = a + b * cos(2. * 3.1415925 * (c * diff + d + time/3.));
       gl_FragColor = vec4(color, 1.0);
     }
   `;
@@ -92,10 +149,10 @@ const sketch = ({ context }) => {
   // Setup a material
   const material = new THREE.ShaderMaterial({
     uniforms: {
-      time: { value: 0 },
-      color: { value: new THREE.Color("#ff665e") }
+      time: { value: 0 }
+      //color: { value: new THREE.Color("#ff665e") }
     },
-    wireframe: true,
+    //wireframe: true,
     vertexShader: vertex,
     fragmentShader: fragment
   });
@@ -117,16 +174,7 @@ const sketch = ({ context }) => {
     },
     // Update & render your scene here
     render({ time }) {
-      //material.uniforms.time.value = time;
-      // geometry.vertices.forEach(vector => {
-      //   let u = (vector.x + 0.5) * Math.PI * 2;
-      //   let v = (vector.y + 0.5) * Math.PI;
-
-
-      //   //v.z = 0.5 * Math.sin(v.y * 50);
-      // });
-      // geometry.verticesNeedsUpdate = true;
-
+      material.uniforms.time.value = time;
       controls.update();
       renderer.render(scene, camera);
     },
