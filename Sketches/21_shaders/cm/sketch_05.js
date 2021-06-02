845d7611e7d4de4676f7b6df40daa960584198b3
Yann Depps <yann.deperrot@protonmail.com>
2021-06-02 11:59:13 +0200

    cm sketch_05 wip

--- Sketches/21_shaders/cm/sketch_05.js
+++ Sketches/21_shaders/cm/sketch_05.js
@@ -1,4 +1,4 @@
-// 48.48
+// 1.05.33
 const canvasSketch = require('canvas-sketch');
 const createShader = require('canvas-sketch-util/shader');
 const glsl = require('glslify');
@@ -9,8 +9,11 @@ const settings = {
   dimensions: [512, 512],
   context: 'webgl',
   animate: true,
-  duration: 8,
-  fps: 30
+  duration: 7,
+  fps: 24,
+  attributes: {
+    antialias: true
+  }
 };
 
 // Your glsl code
@@ -20,8 +23,8 @@ const frag = glsl(`
 
   #define M_PI 3.1415926535897932384626433832795
 
-  // uniform float time;
-  uniform float playhead;
+  uniform float time;
+  // uniform float playhead;
   varying vec2 vUv;
 
   // transform vUv to radial uv
@@ -39,20 +42,52 @@ const frag = glsl(`
     float elevation = cnoise2(vUv * 15.0);
     elevation += cnoise2(vUv * 80.0) * 0.2;
     elevation += cnoise2(vUv * 200.0) * 0.1;
-    elevation += cnoise2(vUv * 900.0) * 0.15;
+    elevation += abs( cnoise2(vUv * 900.0) * 0.15 );
 
     return elevation;
+  }
+
+  // colors
+  vec3 getTerrainColor(float elevation) {
+    vec3 sandColor = vec3(0.96, 0.64, 0.38);
+    vec3 forestColor = vec3(0.0, 0.5, 0.0);
+    vec3 meltedSnowColor = vec3(0.7, 0.9, 1.0);
+    vec3 snowColor = vec3(1.0);
+
+    vec3 color = mix(sandColor, forestColor, elevation);
+
+    if(elevation > 0.7) {
+      color = mix(meltedSnowColor, snowColor, (elevation - 0.7) / 0.3);
+    }
+
+    return color;
+  }
+
+  vec3 getWaterColor(float elevation) {
+    vec3 depthColor = vec3(0.2, 0.54, 0.75) * 0.5;
+    vec3 shoreColor = vec3(0.2, 0.54, 0.75);
+
+    float strength = -elevation;
+
+    vec3 color = mix(shoreColor, depthColor, strength);
+
+    return color;
   }
 
   void main () {
     vec2 pos = vUv - 0.5;
     vec2 radialUv = getRadialUv(pos);
-    radialUv.y -= playhead * 0.05;
-    radialUv.x -= playhead * 0.02;
+    radialUv.y -= time * 0.01;
+    radialUv.x -= time * 0.02;
 
     float elevation = getElevation(radialUv);
 
-    vec3 color = vec3(elevation);
+    vec3 color = vec3(0.0);
+    if(elevation < 0.0) {
+      color = getTerrainColor(elevation);
+    } else {
+      color = getWaterColor(elevation);
+    }
 
     gl_FragColor = vec4(color, 1.0);
   }
@@ -69,8 +104,8 @@ const sketch = ({ gl }) => {
     // Specify additional uniforms to pass down to the shaders
     uniforms: {
       // Expose props from canvas-sketch
-      // time: ({ time }) => time
-      playhead: ({ playhead }) => playhead * (Math.PI * 2)
+      time: ({ time }) => time * Math.PI
+      // playhead: ({ playhead }) => playhead * (Math.PI * 2)
     }
   });
 };
