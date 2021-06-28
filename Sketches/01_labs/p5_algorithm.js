e138a8f28e47206c08b6bc80f55cd84faa9538cf
Yann Depps <yann.deperrot@protonmail.com>
2021-06-28 13:55:30 +0200

    ex 002

--- Sketches/01_labs/p5_algorithm.js
+++ Sketches/01_labs/p5_algorithm.js
@@ -3,11 +3,18 @@
 */
 
 const canvasSketch = require('canvas-sketch');
+const Random = require("canvas-sketch-util/random");
 const p5 = require('p5');
 new p5();
 
+const defaultSeed = 'substrate_062821';
+Random.setSeed(defaultSeed || Random.getRandomSeed());
+console.log('Random Seed:', Random.getSeed());
+
 const settings = {
   p5: true,
+  hotkeys: true,
+  suffix: Random.getSeed(),
   dimensions: [ 512, 512 ],
   animate: true,
   // loop: true,
